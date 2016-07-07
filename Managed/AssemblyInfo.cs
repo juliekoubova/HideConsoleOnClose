e3882319f869e8de6c9e16716158ffd77efe1d08
Julie Koubová <juliekoubova@icloud.com>
2016-07-07 18:40:52 +0200

    nuget package

---
+++ Managed/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Hide Console on Close")]
+
+[assembly: AssemblyDescription("Hide console window when the user attempts to close it, keeping the process running invisibly.")]
+[assembly: AssemblyCopyright("Copyright © 2016 Julie Koubová")]
+[assembly: AssemblyCompany("Julie Koubová")]
+[assembly: AssemblyVersion("0.1.0.0")]
+
+
+
---
+++ Managed/HideConsoleOnClose.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HideConsoleOnCloseManaged
+{
+    public static class HideConsoleOnClose
+    {
+        [DllImport("kernel32")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("HideConsoleOnClose", EntryPoint = "EnableForWindow")]
+        private static extern Boolean EnableForWindow32(IntPtr hWnd);
+
+        [DllImport("HideConsoleOnClose64", EntryPoint = "EnableForWindow")]
+        private static extern Boolean EnableForWindow64(IntPtr hWnd);
+
+        public static Boolean EnableForWindow(IntPtr hWnd)
+        {
+            if (IntPtr.Size == 4)
+            {
+                return EnableForWindow32(hWnd);
+            }
+
+            return EnableForWindow64(hWnd);
+        }
+
+        public static Boolean Enable()
+        {
+            var hWnd = GetConsoleWindow();
+
+            if (hWnd != IntPtr.Zero)
+            {
+                return EnableForWindow(hWnd);
+            }
+
+            return false;
+        }
+    }
+}
